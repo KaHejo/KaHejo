5027dfc3462bb92f190828822fb39324819b0f4e
asataim <asherakmal9361@gmail.com>
2025-04-09 22:52:50 +0700

    fitur kalkulasi carbon emission

--- kahejo/routes/web.php
+++ kahejo/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CarbonFootprintController;
+
+Route::get('/carbon', [CarbonFootprintController::class, 'index']);
+Route::post('/carbon/calculate', [CarbonFootprintController::class, 'calculate']);
 
 Route::get('/', function () {
     return view('welcome');
